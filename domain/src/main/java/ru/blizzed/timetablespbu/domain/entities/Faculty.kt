f17226ff98ffaca0fa0315c6eca913d647f07c66
Ivan Vlasov <Blizzed@bk.ru>
2019-04-10 05:27:54 +0300

    Added mappers for each layouts

--- domain/src/main/java/ru/blizzed/timetablespbu/domain/entities/Faculty.kt
+++ domain/src/main/java/ru/blizzed/timetablespbu/domain/entities/Faculty.kt
@@ -1,3 +1,8 @@
 package ru.blizzed.timetablespbu.domain.entities
 
-data class Faculty(val name: String, val alias: String, var logo: String, val addresses: List<Address>?)
+data class Faculty(
+        val name: String,
+        val alias: String,
+        var logo: String,
+        val addresses: List<Address>?
+)
--- domain/src/main/java/ru/blizzed/timetablespbu/domain/repositories/EducatorsRepository.kt
+++ domain/src/main/java/ru/blizzed/timetablespbu/domain/repositories/EducatorsRepository.kt
@@ -1,10 +1,10 @@
 package ru.blizzed.timetablespbu.domain.repositories
 
 import io.reactivex.Single
-import ru.blizzed.timetablespbu.domain.entities.EducatorEntity
+import ru.blizzed.timetablespbu.domain.entities.Educator
 
 interface EducatorsRepository {
 
-    fun search(query: String): Single<List<EducatorEntity>>
+    fun search(query: String): Single<List<Educator>>
 
 }
