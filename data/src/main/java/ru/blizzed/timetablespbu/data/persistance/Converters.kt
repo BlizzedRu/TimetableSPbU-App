fdd64e248af7235cc2c152b8a355f19249da1838
Ivan Vlasov <Blizzed@bk.ru>
2019-04-14 07:16:46 +0300

    Gson convert exception fixed

--- data/src/main/java/ru/blizzed/timetablespbu/data/persistance/Converters.kt
+++ data/src/main/java/ru/blizzed/timetablespbu/data/persistance/Converters.kt
@@ -2,6 +2,7 @@ package ru.blizzed.timetablespbu.data.persistance
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import ru.blizzed.timetablespbu.data.persistance.entities.EducatorEmploymentData
 
 class Converters {
@@ -14,8 +15,8 @@ class Converters {
     @TypeConverter
     fun convertEducatorEmploymentFrom(stringedItem: String): List<EducatorEmploymentData> = fromJson(stringedItem)
 
-    private inline fun <reified T> toJson(item: T): String = gson.toJson(item, T::class.java)
+    private inline fun <reified T> toJson(item: T): String = gson.toJson(item, object : TypeToken<T>() {}.type)
 
-    private inline fun <reified T> fromJson(item: String): T = gson.fromJson(item, T::class.java)
+    private inline fun <reified T> fromJson(item: String): T = gson.fromJson(item, object : TypeToken<T>() {}.type)
 
 }
