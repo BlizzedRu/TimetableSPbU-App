0cc0ff2dd59765f5b6968a93b32052044ab751eb
Ivan Vlasov <Blizzed@bk.ru>
2019-04-28 21:47:00 +0300

    Educator name separation on last name, first name and patronymic

--- domain/src/main/java/ru/blizzed/timetablespbu/domain/entities/Educator.kt
+++ domain/src/main/java/ru/blizzed/timetablespbu/domain/entities/Educator.kt
@@ -3,13 +3,31 @@ package ru.blizzed.timetablespbu.domain.entities
 import java.util.Objects
 
 class Educator(
-        val id: Int,
-        val fullName: String,
-        val employments: List<EducatorEmployment>,
-        isFavorite: Boolean = false,
-        isViewed: Boolean = false,
-        lastInteractionTime: Long = 0L
+    val id: Int,
+    val fullName: String,
+    val employments: List<EducatorEmployment>,
+    var circleColor: Int = NO_COLOR,
+    isFavorite: Boolean = false,
+    isViewed: Boolean = false,
+    lastInteractionTime: Long = 0L
 ) : Interactable(isFavorite, isViewed, lastInteractionTime) {
+
+    companion object {
+        const val NO_COLOR = 0
+        private const val FULL_NAME_PARTS_SEPARATOR = " "
+    }
+
+    /**
+     * I believe that any educator name is build with this format %lastName firstName? patronymic? ..?%
+     */
+    fun getNameComponents(): NameComponents = fullName.split(FULL_NAME_PARTS_SEPARATOR)
+        .run {
+            NameComponents(
+                lastName = first(),
+                firstName = takeIf { size > 1 }?.get(1),
+                patronymic = takeIf { size > 2 }?.get(2)
+            )
+        }
 
     override fun equals(other: Any?): Boolean {
         if (other == null || other.javaClass != javaClass) return false
---
+++ domain/src/main/java/ru/blizzed/timetablespbu/domain/entities/NameComponents.kt
@@ -0,0 +1,3 @@
+package ru.blizzed.timetablespbu.domain.entities
+
+data class NameComponents(val lastName: String, val firstName: String? = null, val patronymic: String? = null)
