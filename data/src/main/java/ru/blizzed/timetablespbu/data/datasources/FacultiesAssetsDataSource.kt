4fb7fa067304453842d8fa3c53caae21d4068c7e
Ivan Vlasov <Blizzed@bk.ru>
2019-11-06 21:36:21 +0300

    Faculties cache management removed to repository layer

--- data/src/main/java/ru/blizzed/timetablespbu/data/datasources/FacultiesAssetsDataSource.kt
+++ data/src/main/java/ru/blizzed/timetablespbu/data/datasources/FacultiesAssetsDataSource.kt
@@ -2,15 +2,14 @@ package ru.blizzed.timetablespbu.data.datasources
 
 import io.reactivex.Scheduler
 import io.reactivex.Single
-import io.reactivex.subjects.BehaviorSubject
 import ru.blizzed.timetablespbu.data.IOScheduler
 import ru.blizzed.timetablespbu.data.utils.AssetsUtil
 import ru.blizzed.timetablespbu.domain.entities.Faculty
 import javax.inject.Inject
 
 class FacultiesAssetsDataSource @Inject constructor(
-    private val assetsUtil: AssetsUtil,
-    @IOScheduler private val ioScheduler: Scheduler
+        private val assetsUtil: AssetsUtil,
+        @IOScheduler private val ioScheduler: Scheduler
 ) {
 
     companion object {
@@ -18,27 +17,16 @@ class FacultiesAssetsDataSource @Inject constructor(
         private const val FACULTIES_INFO_FILE_NAME = "info.json"
     }
 
-    private var facultiesSubject = BehaviorSubject.create<List<Faculty>>()
-
-    fun getFaculties(): Single<List<Faculty>> {
-        return if (facultiesSubject.hasValue()) {
-            facultiesSubject.singleOrError()
-        } else {
-            fetch()
-        }
-    }
-
-    private fun fetch(): Single<List<Faculty>> = Single
-        .fromCallable {
-            assetsUtil
-                .getListModel(FACULTIES_FOLDER_NAME, FACULTIES_INFO_FILE_NAME, Faculty::class.java)
-                .also { faculties ->
-                    faculties.forEach { faculty ->
-                        faculty.logo = faculty.logo.prependIndent("$FACULTIES_FOLDER_NAME/")
-                    }
-                    facultiesSubject.onNext(faculties)
-                }
-        }
-        .subscribeOn(ioScheduler)
+    fun getFaculties(): Single<List<Faculty>> = Single
+            .fromCallable {
+                assetsUtil
+                        .getListModel(FACULTIES_FOLDER_NAME, FACULTIES_INFO_FILE_NAME, Faculty::class.java)
+                        .also { faculties ->
+                            faculties.forEach { faculty ->
+                                faculty.logo = faculty.logo.prependIndent("$FACULTIES_FOLDER_NAME/")
+                            }
+                        }
+            }
+            .subscribeOn(ioScheduler)
 
 }
--- data/src/main/java/ru/blizzed/timetablespbu/data/repositories/FacultiesDataRepository.kt
+++ data/src/main/java/ru/blizzed/timetablespbu/data/repositories/FacultiesDataRepository.kt
@@ -1,14 +1,26 @@
 package ru.blizzed.timetablespbu.data.repositories
 
 import io.reactivex.Single
+import io.reactivex.subjects.BehaviorSubject
 import ru.blizzed.timetablespbu.data.datasources.FacultiesAssetsDataSource
 import ru.blizzed.timetablespbu.domain.entities.Faculty
 import ru.blizzed.timetablespbu.domain.repositories.FacultiesRepository
 
 class FacultiesDataRepository(
-    private val facultiesDataSource: FacultiesAssetsDataSource
+        private val facultiesDataSource: FacultiesAssetsDataSource
 ) : FacultiesRepository {
 
-    override fun getFaculties(): Single<List<Faculty>> = facultiesDataSource.getFaculties()
+    private val cache = BehaviorSubject.create<List<Faculty>>()
+
+    override fun getAll(): Single<List<Faculty>> = if (cache.hasValue()) cache.singleOrError() else fetch()
+
+    override fun search(query: String): Single<List<Faculty>> = getAll()
+            .map { faculties ->
+                faculties.filter { faculty ->
+                    faculty.name.contains(query, ignoreCase = true) || faculty.alias.contains(query, ignoreCase = true)
+                }
+            }
+
+    private fun fetch() = facultiesDataSource.getFaculties().doOnSuccess(cache::onNext)
 
 }
