f9379eb98c44953b6e1078451d7f69dd92c5841c
Ivan Vlasov <Blizzed@bk.ru>
2019-11-06 21:57:15 +0300

    BehaviorSubject changed to SingleSubject

--- data/src/main/java/ru/blizzed/timetablespbu/data/repositories/FacultiesDataRepository.kt
+++ data/src/main/java/ru/blizzed/timetablespbu/data/repositories/FacultiesDataRepository.kt
@@ -1,7 +1,7 @@
 package ru.blizzed.timetablespbu.data.repositories
 
 import io.reactivex.Single
-import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.SingleSubject
 import ru.blizzed.timetablespbu.data.datasources.FacultiesAssetsDataSource
 import ru.blizzed.timetablespbu.domain.entities.Faculty
 import ru.blizzed.timetablespbu.domain.repositories.FacultiesRepository
@@ -10,9 +10,9 @@ class FacultiesDataRepository(
         private val facultiesDataSource: FacultiesAssetsDataSource
 ) : FacultiesRepository {
 
-    private val cache = BehaviorSubject.create<List<Faculty>>()
+    private val cache = SingleSubject.create<List<Faculty>>()
 
-    override fun getAll(): Single<List<Faculty>> = if (cache.hasValue()) cache.singleOrError() else fetch()
+    override fun getAll(): Single<List<Faculty>> = if (cache.hasValue()) cache else fetch()
 
     override fun search(query: String): Single<List<Faculty>> = getAll()
             .map { faculties ->
@@ -21,6 +21,6 @@ class FacultiesDataRepository(
                 }
             }
 
-    private fun fetch() = facultiesDataSource.getFaculties().doOnSuccess(cache::onNext)
+    private fun fetch() = facultiesDataSource.getFaculties().doOnSuccess(cache::onSuccess)
 
 }
