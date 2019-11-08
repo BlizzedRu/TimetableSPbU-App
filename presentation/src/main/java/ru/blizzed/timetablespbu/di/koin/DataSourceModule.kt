2ccad9cb5590311ccacc28b961fde24c81cf5629
Ivan Vlasov <Blizzed@bk.ru>
2019-11-09 00:23:13 +0300

    Koin data modules

---
+++ presentation/src/main/java/ru/blizzed/timetablespbu/di/koin/DataSourceModule.kt
@@ -0,0 +1,14 @@
+package ru.blizzed.timetablespbu.di.koin
+
+import org.koin.dsl.module
+import ru.blizzed.timetablespbu.data.datasources.*
+
+val dataSourceModule = module {
+    single { AddressesLocalDataSource(get(), get(dbScheduler)) }
+    single { AddressesRemoteDataSource(get()) }
+
+    single { EducatorsLocalDataSource(get(), get(dbScheduler)) }
+    single { EducatorsRemoteDataSource(get()) }
+
+    single { FacultiesAssetsDataSource(get(), get(ioScheduler)) }
+}
---
+++ presentation/src/main/java/ru/blizzed/timetablespbu/di/koin/RepositoriesModule.kt
@@ -0,0 +1,12 @@
+package ru.blizzed.timetablespbu.di.koin
+
+import org.koin.dsl.module
+import ru.blizzed.timetablespbu.data.repositories.EducatorsDataRepository
+import ru.blizzed.timetablespbu.data.repositories.FacultiesDataRepository
+import ru.blizzed.timetablespbu.domain.repositories.EducatorsRepository
+import ru.blizzed.timetablespbu.domain.repositories.FacultiesRepository
+
+val repositoriesModule = module {
+    single<EducatorsRepository> { EducatorsDataRepository(get(), get(), get()) }
+    single<FacultiesRepository> { FacultiesDataRepository(get()) }
+}
