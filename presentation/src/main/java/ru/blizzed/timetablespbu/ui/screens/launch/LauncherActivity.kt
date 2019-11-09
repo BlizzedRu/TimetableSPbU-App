6179aa1163ebb0950fb5671064917480a6aac9f6
Ivan Vlasov <Blizzed@bk.ru>
2019-11-09 03:32:07 +0300

    Created view part of launch screen

---
+++ presentation/src/main/java/ru/blizzed/timetablespbu/ui/screens/launch/LauncherActivity.kt
@@ -0,0 +1,14 @@
+package ru.blizzed.timetablespbu.ui.screens.launch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.blizzed.timetablespbu.R
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+
+}
