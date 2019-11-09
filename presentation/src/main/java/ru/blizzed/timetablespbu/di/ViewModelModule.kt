70e4b562b30a8099b1e87714ea46da8f290408a1
Ivan Vlasov <Blizzed@bk.ru>
2019-11-09 06:00:46 +0300

    Added DI for launcher

--- presentation/src/main/java/ru/blizzed/timetablespbu/di/ViewModelModule.kt
+++ presentation/src/main/java/ru/blizzed/timetablespbu/di/ViewModelModule.kt
@@ -2,12 +2,14 @@ package ru.blizzed.timetablespbu.di
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import ru.blizzed.timetablespbu.ui.screens.launch.LauncherViewModel
 import ru.blizzed.timetablespbu.ui.screens.main.search.faculties.FacultiesSearchViewModel
 import ru.blizzed.timetablespbu.viewmodel.EducatorsSearchViewModel
 import ru.blizzed.timetablespbu.viewmodel.EducatorsViewModel
 import ru.blizzed.timetablespbu.viewmodel.FacultiesViewModel
 
 val viewModelModule = module {
+    viewModel { LauncherViewModel(get()) }
     viewModel { EducatorsSearchViewModel(get(), get()) }
     viewModel { EducatorsViewModel(get(), get()) }
     viewModel { FacultiesViewModel(get(), get()) }
