ce8a2a01e4c49782e33e576241c90a42a2ea7ccb
Ivan Vlasov <Blizzed@bk.ru>
2019-11-10 04:03:04 +0300

    Created blank navigation graph

---
+++ presentation/src/main/java/ru/blizzed/timetablespbu/ui/screens/welcome/EducatorSelfSelectionFragment.kt
@@ -0,0 +1,17 @@
+package ru.blizzed.timetablespbu.ui.screens.welcome
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.blizzed.timetablespbu.R
+
+class EducatorSelfSelectionFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_educator_self_selection, container, false)
+    }
+
+}
---
+++ presentation/src/main/java/ru/blizzed/timetablespbu/ui/screens/welcome/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package ru.blizzed.timetablespbu.ui.screens.welcome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.blizzed.timetablespbu.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
