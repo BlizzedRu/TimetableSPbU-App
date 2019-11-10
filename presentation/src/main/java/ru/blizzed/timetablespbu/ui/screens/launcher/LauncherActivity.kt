59c3b9e03edf7da34ff9da5287519c88b0b00383
Ivan Vlasov <Blizzed@bk.ru>
2019-11-10 06:02:34 +0300

    Fixed naming of launcher package

---
+++ presentation/src/main/java/ru/blizzed/timetablespbu/ui/screens/launcher/LauncherActivity.kt
@@ -0,0 +1,45 @@
+package ru.blizzed.timetablespbu.ui.screens.launcher
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.blizzed.timetablespbu.R
+import ru.blizzed.timetablespbu.ui.screens.main.MainActivity
+import ru.blizzed.timetablespbu.ui.screens.welcome.WelcomeActivity
+
+class LauncherActivity : AppCompatActivity() {
+
+    private val viewModel: LauncherViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+
+        viewModel.dispatchEvent(ViewEvent.AppStarted)
+
+        viewModel.observeState(this, ::renderState)
+    }
+
+    private fun renderState(state: ViewState) {
+        with(state) {
+            when {
+                isError -> finish()
+                !isLoading && data != null -> {
+                    val isLoggedIn = data
+
+                    if (isLoggedIn) {
+                        navigateToMain()
+                    } else {
+                        navigateToAuth()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun navigateToMain() = MainActivity.start(this)
+
+    private fun navigateToAuth() = startActivity(Intent(this, WelcomeActivity::class.java))
+
+}
