6f15a5f5c35e26d0cd8a14694dbfa51d6e7bf905
Ivan Vlasov <Blizzed@bk.ru>
2020-04-12 02:32:13 +0300

    Add divisions local data source in di

--- presentation/src/main/java/ru/blizzed/timetablespbu/di/DataSourceModule.kt
+++ presentation/src/main/java/ru/blizzed/timetablespbu/di/DataSourceModule.kt
@@ -1,7 +1,14 @@
 package ru.blizzed.timetablespbu.di
 
 import org.koin.dsl.module
-import ru.blizzed.timetablespbu.data.datasources.*
+import ru.blizzed.timetablespbu.data.datasources.AddressesLocalDataSource
+import ru.blizzed.timetablespbu.data.datasources.AddressesRemoteDataSource
+import ru.blizzed.timetablespbu.data.datasources.DivisionsLocalDataSource
+import ru.blizzed.timetablespbu.data.datasources.DivisionsRemoteDataSource
+import ru.blizzed.timetablespbu.data.datasources.EducatorsLocalDataSource
+import ru.blizzed.timetablespbu.data.datasources.EducatorsRemoteDataSource
+import ru.blizzed.timetablespbu.data.datasources.FacultiesAssetsDataSource
+import ru.blizzed.timetablespbu.data.datasources.UserInfoDataSource
 
 val dataSourceModule = module {
     single { AddressesLocalDataSource(get(), get(dbScheduler)) }
@@ -15,4 +22,5 @@ val dataSourceModule = module {
     single { UserInfoDataSource(get(), get(dbScheduler)) }
 
     single { DivisionsRemoteDataSource(get(), get(ioScheduler)) }
+    single { DivisionsLocalDataSource() }
 }
