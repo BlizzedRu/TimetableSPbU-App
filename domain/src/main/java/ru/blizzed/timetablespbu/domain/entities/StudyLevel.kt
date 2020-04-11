41419f5a5b3aff16ead0ae7123507fa7f5afad46
Ivan Vlasov <Blizzed@bk.ru>
2020-04-12 01:36:42 +0300

    Add id val for study level

--- domain/src/main/java/ru/blizzed/timetablespbu/domain/entities/StudyLevel.kt
+++ domain/src/main/java/ru/blizzed/timetablespbu/domain/entities/StudyLevel.kt
@@ -1,16 +1,17 @@
 package ru.blizzed.timetablespbu.domain.entities
 
 data class StudyLevel(
-        val name: String,
-        val studyProgramCombinations: List<StudyProgramCombination>
+  val id: Int,
+  val name: String,
+  val studyProgramCombinations: List<StudyProgramCombination>
 )
 
 data class StudyProgramCombination(
-        val name: String,
-        val admissionYears: List<AdmissionYear>
+  val name: String,
+  val admissionYears: List<AdmissionYear>
 )
 
 data class AdmissionYear(
-        val studyProgramId: Int,
-        val year: Int
+  val studyProgramId: Int,
+  val year: Int
 )
--- domain/src/main/java/ru/blizzed/timetablespbu/domain/mappers/StudyLevelMappers.kt
+++ domain/src/main/java/ru/blizzed/timetablespbu/domain/mappers/StudyLevelMappers.kt
@@ -10,15 +10,19 @@ import ru.blizzed.timetablespbulib.model.divisions.StudyProgramCombination as Ap
 import ru.blizzed.timetablespbulib.model.divisions.StudyProgramCombination.AdmissionYear as ApiAdmissionYear
 
 object StudyLevelMapper : Mapper<ApiStudyLevel, StudyLevel> {
-    override fun ApiStudyLevel.mapToEntity(): StudyLevel = StudyLevel(studyLevelName, studyProgramCombinations.mapToEntity())
+  override fun ApiStudyLevel.mapToEntity(): StudyLevel =
+    StudyLevel(-1, studyLevelName, studyProgramCombinations.mapToEntity())
+
+  override fun List<ApiStudyLevel>.mapToEntity(): List<StudyLevel> = mapIndexed { index, studyLevel ->
+    StudyLevel(index, studyLevel.studyLevelName, studyLevel.studyProgramCombinations.mapToEntity())
+  }
 }
 
 object AdmissionYearMapper : Mapper<ApiAdmissionYear, AdmissionYear> {
-    override fun ApiAdmissionYear.mapToEntity(): AdmissionYear = AdmissionYear(studyProgramId, yearNumber)
+  override fun ApiAdmissionYear.mapToEntity(): AdmissionYear = AdmissionYear(studyProgramId, yearNumber)
 }
 
 object StudyProgramCombinationMapper : Mapper<ApiStudyProgramCombination, StudyProgramCombination> {
-    override fun ApiStudyProgramCombination.mapToEntity(): StudyProgramCombination = StudyProgramCombination(name, admissionYears.mapToEntity())
-
-
+  override fun ApiStudyProgramCombination.mapToEntity(): StudyProgramCombination =
+    StudyProgramCombination(name, admissionYears.mapToEntity())
 }
